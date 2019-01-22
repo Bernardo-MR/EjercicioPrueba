6725d63a5e523371dbdf8c071d4fc8b924a22765
Bernardo-MR <46934756+Bernardo-MR@users.noreply.github.com>
2019-01-22 11:05:17 -0600

    Bernardo Mondragón Ramírez, Grupo 3

---
+++ EjercicioCoche.py
@@ -0,0 +1,9 @@
+# Autor: Bernardo Mondragon Ramirez
+# Calcula velocidad promedio, dado el tiempo y la distancia
+
+d = int(input("Teclea la distancia en km. enteros: "))
+t = int(input("Teclea el tiempo en hrs. enteros: "))
+
+v = d*t
+
+print("Velocidad promedio:",v, "km/h")
